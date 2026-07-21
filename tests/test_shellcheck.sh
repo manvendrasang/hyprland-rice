139d362bd6e7dffb3011c8a53f92974a55f13c00
Manvendra <manvendrasang@gmail.com>
2026-07-21 20:01:06 +0530

    shellcheck updated

--- tests/test_shellcheck.sh
+++ tests/test_shellcheck.sh
@@ -8,8 +8,8 @@ while IFS= read -r -d '' file; do
     echo "Checking $file"
 
     if ! shellcheck \
-    --rcfile=.shellcheckrc \
     -x \
+    -e SC1090,SC1091,SC2010,SC2015,SC2034,SC2086 \
     "$file"; then
         FAILED=1
     fi
